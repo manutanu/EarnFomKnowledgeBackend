322918d5b5a2e55cb69563e88b9e5b2a10f9d0d3
Mritunjay <Mritunjay.yadav@mindfiresolutions.com>
2019-07-19 17:35:02 +0530

    new barebone project structure

---
+++ src/main/java/com/knowledgeQuizApp/KnowledgeQuizApp/KnowledgeQuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.knowledgeQuizApp.KnowledgeQuizApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeQuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeQuizAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knowledgeQuizApp/KnowledgeQuizApp/KnowledgeQuizAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.knowledgeQuizApp.KnowledgeQuizApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KnowledgeQuizAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
